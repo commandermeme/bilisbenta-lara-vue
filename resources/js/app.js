869ab09076fc672a031e449e38651f3526f1b36f
Cossette Alexis Gabuya <gabuyalexis@gmail.com>
2020-07-13 04:23:40 +0800

    Trying Fixes

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,22 +1,16 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import VueRouter from 'vue-router';
+import VueScrollTo from 'vue-scrollto'
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 Vue.use(VueScrollTo);
 Vue.use(VueRouter);
-=======
-require('./bootstrap');
->>>>>>> parent of d34315a... Added Vue UI
 
-window.Vue = require('vue');
+Vue.config.productionTip = false
 
-<<<<<<< HEAD
 new Vue({
     created() {
         AOS.init();
@@ -24,54 +18,3 @@ new Vue({
     router,
     render: h => h(App)
 }).$mount('#app')
-=======
-=======
-require('./bootstrap');
-
-window.Vue = require('vue');
-
->>>>>>> parent of d34315a... Added Vue UI
-=======
-require('./bootstrap');
-
-window.Vue = require('vue');
-
->>>>>>> parent of d34315a... Added Vue UI
-=======
-require('./bootstrap');
-
-window.Vue = require('vue');
-
->>>>>>> parent of d34315a... Added Vue UI
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
-
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-const app = new Vue({
-    el: '#app',
-});
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> parent of d34315a... Added Vue UI
-=======
->>>>>>> parent of d34315a... Added Vue UI
-=======
->>>>>>> parent of d34315a... Added Vue UI
-=======
->>>>>>> parent of d34315a... Added Vue UI
